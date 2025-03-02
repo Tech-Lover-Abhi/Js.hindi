42244defb99d1387a669badb7a561f3a99f799cf
Tech-Lover-Abhi <nayakabhijit.2004@gmail.com>
2025-03-02 20:25:58 +0530

    Variables

---
+++ 01-Basic/01-variables.js
@@ -0,0 +1,14 @@
+const acountId = 12554565
+let accountEmail = "ahgszdjg@google.com"
+var accountCity = "jaipur"
+accountPassword = "124578"
+
+accountEmail = "Abhi@gkhfs"
+accountCity = "Bangaluru"
+accountPassword = "369"
+
+
+//acountId = 9
+
+console.log(accountEmail);
+console.table([acountId, accountEmail, accountCity, accountPassword])
---
+++ 01-Basic/03-conversionOperation.js
@@ -0,0 +1,14 @@
+let score = ""
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueIntNumber = Boolean(score)
+console.log(typeof valueIntNumber);
+console.log(valueIntNumber);
+
+
+
+// "33" => 33
+//"33abc" => NaN
+// true => 1; falsh => 0;
