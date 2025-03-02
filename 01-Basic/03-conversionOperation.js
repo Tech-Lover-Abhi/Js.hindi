d9caef635ab14092ee83b2c2733f2dc6ec4623a0
Tech-Lover-Abhi <nayakabhijit.2004@gmail.com>
2025-03-02 22:54:58 +0530

    Some new comparison

--- 01-Basic/03-conversionOperation.js
+++ 01-Basic/03-conversionOperation.js
@@ -1,11 +1,11 @@
 let score = ""
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueIntNumber = Boolean(score)
-console.log(typeof valueIntNumber);
-console.log(valueIntNumber);
+// console.log(typeof valueIntNumber);
+// console.log(valueIntNumber);
 
 
 
@@ -13,3 +13,21 @@ console.log(valueIntNumber);
 //"33abc" => NaN
 // true => 1; falsh => 0;
 // "" => falsh; "Abhi" => true;
+
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
